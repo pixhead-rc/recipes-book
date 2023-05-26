1feb43881494100d3645e6ccaf86acffbec03774
pixhead-rc <pokeee2@gmail.com>
2023-05-27 02:15:00 +0700

    search w/ rxjs

--- src/app/recipes/components/recipes-list/recipes-list.component.ts
+++ src/app/recipes/components/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Subscription, debounceTime, distinctUntilChanged, filter } from 'rxjs';
 import { RecipesListItem } from 'src/app/_core/models/recipesListItem';
 import { RecipesService } from 'src/app/_core/services/recipes.service';
 
@@ -9,6 +10,15 @@ import { RecipesService } from 'src/app/_core/services/recipes.service';
 })
 export class RecipesListComponent implements OnInit {
   recipes: RecipesListItem[] = [];
+  filteredRecipes: RecipesListItem[] = [];
+
+  _search!: string;
+  _search$ = new BehaviorSubject(this._search);
+  searchSubscription!: Subscription;
+  set search(value: string) {
+    this._search = value;
+    this._search$.next(this._search);
+  }
 
   currentSortingSetting: string = 'Сначала недорогие';
   sortDropdownExpanded: boolean = false;
@@ -21,10 +31,30 @@ export class RecipesListComponent implements OnInit {
     this.recipesService.getRecipesList().subscribe(
       response => {
         this.recipes = response;
+        this.filteredRecipes = response;
         for (let i = 0; i < 20; i++) {
           this.recipes.push(response[0]);
         }
       }
+    );
+    this.searchSubscription = this._search$.asObservable().pipe(
+      filter(searchText => !!searchText && searchText.length > 2 || searchText === ''),
+      debounceTime(500),
+      distinctUntilChanged(),
+    ).subscribe(
+      searchText => {
+        this.filteredRecipes = this.filterInList(searchText);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.searchSubscription.unsubscribe();
+  }
+
+  filterInList(text: string) {
+    return this.recipes.filter(
+      recipe => recipe.title.toLocaleLowerCase().includes(text.toLowerCase())
     );
   }
 
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -4,10 +4,12 @@ import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 import { RecipeViewComponent } from './components/recipe-view/recipe-view.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RecipesRoutingModule
   ],
   declarations: [
